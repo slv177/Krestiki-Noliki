bd479decfbd444110c7ff0bd3ad5d3df73a99a2e
Viacheslav Shambazov <SS@SS.local>
2015-05-16 12:30:33 +0300

    Initial Commit

---
+++ Krestiki-Noliki/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Krestiki-Noliki
+//
+//  Created by Viacheslav Shambazov on 16/05/2015.
+//  Copyright (c) 2015 Viacheslav Shambazov. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
